c28c99afbf3ff31ac52f8d6fddcd22e82643ac02
JR <zorael@gmail.com>
2019-07-16 18:12:08 +0200

    Initial code dump, Windows only

---
+++ source/app.d
@@ -0,0 +1,122 @@
+import std.stdio;
+
+version(Windows) {}
+else
+{
+	static assert(0, "This only works on Windows as of yet");
+
+}
+
+long[string] pidTime;
+
+enum sleepTime = 10;  // seconds
+enum timeoutTime = 60;  // seconds
+
+
+void main()
+{
+	while (true)
+	{
+		updatePidTable();
+
+		immutable now = Clock.currTime.toUnixTime;
+		string[] toRemove;
+
+		foreach (pid, timestamp; pidTime)
+		{
+			if ((now - timestamp) > timeoutTime)
+			{
+				writeln("Killing ", pid);
+				exec("taskkill " ~ pid);
+				toRemove ~= pid;
+			}
+		}
+
+		foreach (pid; toRemove)
+		{
+			pidTime.remove(pid);
+		}
+	}
+
+	Thread.sleep(sleepTime.seconds);
+}
+
+
+void updatePidTable(const string filename)
+{
+	import std.conv : to;
+	import std.datetime.systime : Clock;
+	import std.process : execute;
+	import std.string : stripLeft;
+
+	immutable tasklist = exec("tasklist /FI IMAGENAME eq " ~ filename);
+
+	auto pidList = tasklist.output.splitter("\n");
+	while (!pidList.front.startsWith(filename)) pidList.popFront();
+
+	foreach (immutable pidLine; pidList)
+	{
+		string line = pidLine;  // mutable
+		immutable cmd = line.nom(' ');
+		immutable rest = line.stripLeft();
+		immutable pid = rest.nom(' ');
+
+		if (pid !in pidTime)
+		{
+			writeln("Saw new pid ", pid);
+			pidTime[pid] = Clock.currTime.toUnixTime;
+		}
+	}
+}
+
+
+pragma(inline)
+T nom(Flag!"decode" decode = No.decode, T, C)(auto ref T line, const C separator,
+    const string callingFile = __FILE__, const size_t callingLine = __LINE__) pure
+if (isMutable!T && isSomeString!T && (is(C : T) || is(C : ElementType!T) || is(C : ElementEncodingType!T)))
+{
+    static if (decode || is(T : dstring) || is(T : wstring))
+    {
+        import std.string : indexOf;
+        // dstring and wstring only work with indexOf, not countUntil
+        immutable index = line.indexOf(separator);
+    }
+    else
+    {
+        // Only do this if we know it's not user text
+        import std.algorithm.searching : countUntil;
+        import std.string : representation;
+
+        static if (isSomeString!C)
+        {
+            immutable index = line.representation.countUntil(separator.representation);
+        }
+        else
+        {
+            immutable index = line.representation.countUntil(cast(ubyte)separator);
+        }
+    }
+
+    if (index == -1)
+    {
+        import std.format : format;
+        throw new Exception(`Tried to nom too much: "%s" with "%s"`
+            .format(line, separator), callingFile, callingLine);
+    }
+
+    static if (isSomeString!C)
+    {
+        immutable separatorLength = separator.length;
+    }
+    else
+    {
+        enum separatorLength = 1;
+    }
+
+    static if (__traits(isRef, line))
+    {
+        scope(exit) line = line[(index+separatorLength)..$];
+    }
+
+    return line[0..index];
+}
